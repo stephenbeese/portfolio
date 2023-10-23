33383c812f90c3235e0b55a85bad30c5996d4a64
stephenbeese <stephenbeese1@live.co.uk>
2023-10-23 16:38:57 +0000

    Add images and descriptions

--- js/custom.js
+++ js/custom.js
@@ -14,3 +14,35 @@ document.querySelectorAll('.portfolio-item').forEach(function (item, index) {
         captionContents[index].style.display = "none";
     });
 });
+
+
+
+// Contact form
+
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById("contact-form");
+
+    contactForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        // Send the form data to Formspree
+        fetch("https://formspree.io/stephenbeese1@live.co.uk", {
+            method: "POST",
+            body: new FormData(contactForm),
+            headers: {
+                Accept: "application/json",
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.ok) {
+                // The email was sent successfully
+                alert("Your message has been sent. Thank you!");
+                contactForm.reset();
+            } else {
+                // There was an error
+                alert("There was an error. Please try again later.");
+            }
+        });
+    });
+});
